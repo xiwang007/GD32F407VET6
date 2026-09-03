3fc8dcc1fcfbb705c56e7f8e9fd717b3e114c01c
xiwang007 <394227933@qq.com>
2026-09-03 13:21:21 +0800

    切换输入输出

--- MAL/MCU/ExtModules/GPIO/jk_gpio_switch.c
+++ MAL/MCU/ExtModules/GPIO/jk_gpio_switch.c
@@ -1,3 +1,57 @@
 #include "jk_gpio_switch.h"
 
+// 设置最开始的 输入/输出 状态
+void JK_GPIO_SWITCH_T_BASE_F_Init(JK_GPIO_SWITCH_T_BASE *self)
+{
+    if (self->type == JK_GPIO_SWITCH_E_TYPE_Input)
+    {
+        // 输入
+        JK_GPIO_I_T_BASE_F_Init(&self->input);
+        self->type = JK_GPIO_SWITCH_E_TYPE_Input;
+    }
+    else
+    {
+        // 输出
+        JK_GPIO_O_T_BASE_F_Init(&self->output);
+        self->type = JK_GPIO_SWITCH_E_TYPE_Output;
+    }
+}
 
+// 切换 输入/输出
+void JK_GPIO_SWITCH_T_BASE_F_Switch(JK_GPIO_SWITCH_T_BASE *self, JK_GPIO_SWITCH_E_TYPE type)
+{
+    self->type = type;
+    JK_GPIO_SWITCH_T_BASE_F_Init(self);
+}
+
+// 输入获取当前的状态
+void JK_GPIO_SWITCH_T_BASE_F_IGetBit(JK_GPIO_SWITCH_T_BASE *self)
+{
+    if (self->type != JK_GPIO_SWITCH_E_TYPE_Input)
+        return;
+    JK_GPIO_I_T_BASE_F_GetBit(&self->input);
+}
+
+// 输出设置为高电平
+void JK_GPIO_SWITCH_T_BASE_F_OSet(JK_GPIO_SWITCH_T_BASE *self)
+{
+    if (self->type != JK_GPIO_SWITCH_E_TYPE_Output)
+        return;
+    JK_GPIO_O_T_BASE_F_Set(&self->output);
+}
+
+// 输出设置为低电平
+void JK_GPIO_SWITCH_T_BASE_F_OReset(JK_GPIO_SWITCH_T_BASE *self)
+{
+    if (self->type != JK_GPIO_SWITCH_E_TYPE_Output)
+        return;
+    JK_GPIO_O_T_BASE_F_Reset(&self->output);
+}
+
+// 输出切换高低电平
+void JK_GPIO_SWITCH_T_BASE_F_OToggle(JK_GPIO_SWITCH_T_BASE *self)
+{
+    if (self->type != JK_GPIO_SWITCH_E_TYPE_Output)
+        return;
+    JK_GPIO_O_T_BASE_F_Toggle(&self->output);
+}
--- MAL/MCU/ExtModules/GPIO/jk_gpio_switch.h
+++ MAL/MCU/ExtModules/GPIO/jk_gpio_switch.h
@@ -1,6 +1,31 @@
 #ifndef __JK_GPIO_SWITCH_H
 #define __JK_GPIO_SWITCH_H
 
-#include "gd32f4xx.h"
+#include "jk_gpio.h"
+#include "jk_gpio_i.h"
+#include "jk_gpio_o.h"
+
+typedef enum JK_GPIO_SWITCH_E_Type
+{
+    JK_GPIO_SWITCH_E_TYPE_Output = 0,
+    JK_GPIO_SWITCH_E_TYPE_Input = 1,
+
+} JK_GPIO_SWITCH_E_TYPE;
+
+typedef struct
+{
+    JK_GPIO_I_T_BASE input;  // 输入
+    JK_GPIO_O_T_BASE output; // 输出
+
+    JK_GPIO_SWITCH_E_TYPE type;// 当前是切换到输入还是输出
+
+} JK_GPIO_SWITCH_T_BASE;
+
+void JK_GPIO_SWITCH_T_BASE_F_Init(JK_GPIO_SWITCH_T_BASE *self);
+void JK_GPIO_SWITCH_T_BASE_F_Switch(JK_GPIO_SWITCH_T_BASE *self, JK_GPIO_SWITCH_E_TYPE type);
+void JK_GPIO_SWITCH_T_BASE_F_IGetBit(JK_GPIO_SWITCH_T_BASE *self);
+void JK_GPIO_SWITCH_T_BASE_F_OSet(JK_GPIO_SWITCH_T_BASE *self);
+void JK_GPIO_SWITCH_T_BASE_F_OReset(JK_GPIO_SWITCH_T_BASE *self);
+void JK_GPIO_SWITCH_T_BASE_F_OToggle(JK_GPIO_SWITCH_T_BASE *self);
 
 #endif
